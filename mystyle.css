21116a307e519e33b58487c2000b3ee57998d7e7
ParisyaNaylah <parisyanaylah@gmail.com>
2024-09-02 15:33:43 +0700

    Pertemuan 2

---
+++ mystyle.css
@@ -0,0 +1,38 @@
+body {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: beige;
+    font-size: 40px;
+}
+a:link {
+    text-decoration: none;
+    color :blue;
+}
+a:visited {
+    text-decoration: none;
+    color :blue;
+}
+a:hover {
+    text-decoration: none;
+    color :yellow;
+    text-decoration: underline;
+}
+
+.headingmerah {
+    color:red;
+}
+
+.parratakanan {
+    text-align: right;
+}
+
+.partengah {
+    text-align: center;
+}
+
+.rcorners2 {
+    border-radius: 25px;
+    border: 2px solid #73AD21;
+    padding: 20px;
+    width: 200px;
+    height: 250px;
+  }
